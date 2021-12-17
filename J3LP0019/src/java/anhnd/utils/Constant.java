8bff1abc8e68b0cad10eab863f7b93999bf0bfd2
Duy Anh Nguyễn <55968905+nguynduyanh169@users.noreply.github.com>
2021-12-17 08:46:53 +0700

    Update Constant.java

--- J3LP0019/src/java/anhnd/utils/Constant.java
+++ J3LP0019/src/java/anhnd/utils/Constant.java
@@ -11,8 +11,8 @@ package anhnd.utils;
  */
 public class Constant {
 
-    public static String GOOGLE_CLIENT_ID = "969760503071-d1kvfvnsaef338umgkd2c2tv31e4rflh.apps.googleusercontent.com";
-    public static String GOOGLE_CLIENT_SECRET = "4VnaQjVFdMFfl5q5BDExB0gI";
+    public static String GOOGLE_CLIENT_ID = "Google_client_id";
+    public static String GOOGLE_CLIENT_SECRET = "Google_client_secret";
     public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/J3LP0019/loginGoogle";
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
     public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
